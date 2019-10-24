20d5560643236cec51220baac1c29bd193f18130
julio-crypt0 <hernandezjulio58@ymail.com>
2019-10-23 17:58:56 -0700

    Updates

---
+++ precache-manifest.2dce7f3ba1c6f1d86c54cedfcde4131f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1562004ca1dae6fd746940703bbfc0f6",
+    "url": "/julio-github-searcher/index.html"
+  },
+  {
+    "revision": "42dcb20c122e9b89a2fc",
+    "url": "/julio-github-searcher/static/css/main.7c182a69.chunk.css"
+  },
+  {
+    "revision": "5821a112fcf9a3c17ac0",
+    "url": "/julio-github-searcher/static/js/2.49df413d.chunk.js"
+  },
+  {
+    "revision": "42dcb20c122e9b89a2fc",
+    "url": "/julio-github-searcher/static/js/main.c70d9387.chunk.js"
+  },
+  {
+    "revision": "6036f73fcfb1845963ea",
+    "url": "/julio-github-searcher/static/js/runtime-main.862482a1.js"
+  },
+  {
+    "revision": "3176ac7dda13c19e99a9e856c0e66123",
+    "url": "/julio-github-searcher/static/media/videoplayback.3176ac7d.webm"
+  }
+]);
